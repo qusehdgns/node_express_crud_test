78daf9a20291bac10650489285bb796e4d47e624
qusehdgns <qusehdgns123@gmail.com>
2021-03-15 17:47:36 +0900

    js api crud

--- server.js
+++ server.js
@@ -5,23 +5,12 @@ app.use(express.json());
 
 const port = process.env.PORT || 8000;
 
-// 404 오류
-app.use(function (req, res, next) {
-    res.status(404).send('404 에러');
-});
-
-// 500 애러
-app.use(function (err, req, res, next) {
-    console.error(err.stack)
-    res.status(500).send('500 에러')
-});
-
 app.listen(port, () => {
     console.log(`${port}번 port에 http server를 띄웠습니다.`);
 });
 
 app.get('/', (req, res) => {
-    res.send(`express.js로 만든 server입니다`);
+    res.sendFile(__dirname + "/fetch_test.html");
 });
 
 const temp = [
@@ -37,50 +26,50 @@ app.get('/crud/test', (req, res) => {
 app.post('/crud/test', (req, res) => {
     // 데이터 유효성 검사
     if(!req.body.name || req.body.name.length < 3){
-        res.status(400).send('이름 규칙이 어긋남');
+        res.status(400).send({ error : '이름 규칙이 어긋남' });
         return;
     }
 
 
     let temp_case = {
-        id : temp.length + 1,
+        id : Number(req.body.id),
         name : req.body.name
     };
     temp.push(temp_case);
-    res.send(temp);
+    res.send(temp_case);
 })
 
 app.put('/crud/test/:id', (req, res) => {
     let temp_case = temp.find(t => t.id === parseInt(req.params.id))
     if(!temp){
-        res.status(404).send("해당 id가 없음")
+        res.status(404).send({ error : "해당 id가 없음" })
     }
 
     if(!req.body.name || req.body.name.length < 3){
-        res.status(400).send('이름 규칙이 어긋남');
+        res.status(400).send({ error : '이름 규칙이 어긋남' });
         return;
     }
 
     temp_case.name = req.body.name;
-    res.send(temp);
+    res.send(temp_case);
 })
 
 app.delete('/crud/test/:id', (req, res) => {
     let temp_case = temp.find(c => c.id === parseInt(req.params.id));
     if(!temp_case){
-        res.status(404).send('해당 id가 없음');
+        res.status(404).send({ error : '해당 id가 없음' });
         return;
     }
 
     temp.splice(temp.indexOf(temp_case), 1);
 
-    res.send(temp);
+    res.send(temp_case);
 })
 
 app.get('/crud/test/:id', (req, res) => {
     let temp_case = temp.find(t => t.id === parseInt(req.params.id))
     if(!temp_case){
-        res.status(404).send('The temp with the given ID was not found');
+        res.status(404).send({ error : 'The temp with the given ID was not found' });
     }
 
     res.send(temp_case);
